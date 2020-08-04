9acfc1d2f5de7a477e9987d70ec4410b617b4f9c
leitegs <leitedasilvag@gmail.com>
2020-08-04 13:14:00 -0300

    add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 import routes from './routes'
 
 const app = express();
 
+app.use(cors({
+    origin: 'localhost:8000'
+}))
 app.use(express.json())
 app.use(routes)
 
