665560c14ef519d95983cbc1a6a7688ee7be40fd
leitegs <leitedasilvag@gmail.com>
2020-08-05 09:38:02 -0300

    create form of register

--- web/src/components/pageHeader/index.tsx
+++ web/src/components/pageHeader/index.tsx
@@ -11,6 +11,7 @@ import backIcon from '../../assets/images/icons/back.svg';
 
 interface PageHeaderProps {
     title: string;
+    description?: string;
 }
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
     return (
@@ -23,6 +24,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
             </div>
             <div className="header-content">
                 <strong>{props.title}</strong>
+                {props.description && <p>{props.description}</p>}
 
                 {props.children}
             </div>
